81202316dc75fb9690143b08d6004a7cf21acd1c
brunoscaramelli <50555407+brunoscaramelli@users.noreply.github.com>
2024-12-18 10:09:53 -0300

    1

---
+++ styles/APM_Batalha_CART_6_style.js
@@ -0,0 +1,30 @@
+var size = 0;
+var placement = 'point';
+
+var style_APM_Batalha_CART_6 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; var value = feature.get("");
+    var labelFont = "13.0px \'Open Sans\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "#fafafa";
+    var bufferWidth = 1.0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("Nome") !== null) {
+        labelText = String(feature.get("Nome"));
+    }
+    var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(21,220,251,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 4.028}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
